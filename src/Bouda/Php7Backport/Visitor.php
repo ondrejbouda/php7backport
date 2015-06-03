7552ad3f3457ca94fe360d10842982951ecb9b80
Ondřej Bouda <ondrej.bouda@gmail.com>
2015-06-03 17:46:02 +0200

    refactoring: function header position

--- src/Bouda/Php7Backport/Visitor.php
+++ src/Bouda/Php7Backport/Visitor.php
@@ -3,6 +3,7 @@
 namespace Bouda\Php7Backport;
 
 use PhpParser;
+use PhpParser\Node\Stmt;
 
 
 abstract class Visitor extends PhpParser\NodeVisitorAbstract
@@ -16,5 +17,48 @@ abstract class Visitor extends PhpParser\NodeVisitorAbstract
     {
         $this->tokens = $tokens;
         $this->changedNodes = $changedNodes;
+    }
+
+
+    /**
+     * Find end position of function header declaration in original code 
+     * and set to node attribute.
+     */
+    protected function setOriginalEndOfFunctionHeaderPosition(Stmt $node)
+    {
+        $currentTokenPosition = $node->getAttribute('startTokenPos');
+
+        $offset = 0;
+
+
+        // find first occurence of "{" (start of body)
+        do
+        {
+            $currentToken = $this->tokens[$currentTokenPosition];
+
+            $offset += is_array($currentToken) ? strlen($currentToken[1]) : strlen($currentToken);
+            
+            $currentTokenPosition++;
+        }
+        while ($currentToken != "{");
+
+        $currentTokenPosition--;
+        $currentToken = $this->tokens[$currentTokenPosition];
+        $offset -= is_array($currentToken) ? strlen($currentToken[1]) : strlen($currentToken);
+
+        $currentTokenPosition--;
+        $currentToken = $this->tokens[$currentTokenPosition];
+        if (is_array($currentToken) && $currentToken[0] == T_WHITESPACE)
+        {
+            $offset -= is_array($currentToken) ? strlen($currentToken[1]) : strlen($currentToken);
+        }
+
+
+        $endFilePos =  $node->getAttribute('startFilePos') + $offset;
+
+        // lower by 1 to stay consistent with original (wrong) values by parser
+        $endFilePos -= 1;
+
+        $node->setAttribute('endFilePos', $endFilePos);
     }
 }
--- src/Bouda/Php7Backport/Visitor/Constructor.php
+++ src/Bouda/Php7Backport/Visitor/Constructor.php
@@ -23,7 +23,7 @@ class Constructor extends Php7Backport\Visitor
                 if ($stmt instanceof ClassMethod && $stmt->name == $className)
                 {
                     $changedNode = $this->transform($stmt);
-                    $this->setOriginalEndOfHeaderPosition($stmt, $this->tokens);
+                    $this->setOriginalEndOfFunctionHeaderPosition($stmt);
 
                     $this->changedNodes->addNode($changedNode);
                 }
@@ -49,37 +49,5 @@ class Constructor extends Php7Backport\Visitor
         $node->setAttribute('changed', true);
 
         return new ChangedNode($node);
-    }
-
-
-    /**
-     * Find end position of function header declaration in original code 
-     * and set to node attribute.
-     */
-    private function setOriginalEndOfHeaderPosition(ClassMethod $node, array $tokens)
-    {
-        $currentTokenPosition = $node->getAttribute('startTokenPos');
-
-        $offset = 0;
-
-
-        // find first occurence of ")" (end of header declaration)
-        do
-        {
-            $currentToken = $tokens[$currentTokenPosition];
-
-            $offset += is_array($currentToken) ? strlen($currentToken[1]) : strlen($currentToken);
-            
-            $currentTokenPosition++;
-        }
-        while ($currentToken != ")");
-
-
-        $endFilePos =  $node->getAttribute('startFilePos') + $offset;
-
-        // lower by 1 to stay consistent with original (wrong) values by parser
-        $endFilePos -= 1;
-
-        $node->setAttribute('endFilePos', $endFilePos);
     }
 }

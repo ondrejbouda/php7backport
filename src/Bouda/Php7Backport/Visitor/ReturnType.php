b87d20954856d1a74ea2e980e7ef0569fff03179
Ondřej Bouda <ondrej.bouda@gmail.com>
2015-07-15 14:46:24 +0200

    new version of php parser; FunctionLike interface

--- src/Bouda/Php7Backport/Visitor/ReturnType.php
+++ src/Bouda/Php7Backport/Visitor/ReturnType.php
@@ -7,9 +7,8 @@ use Bouda\Php7Backport\Patch;
 use Bouda\Php7Backport\Printer\FunctionHeaderPrinter;
 
 use PhpParser\Node;
+use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Stmt;
-use PhpParser\Node\Stmt\Function_;
-use PhpParser\Node\Stmt\ClassMethod;
 
 
 /**
@@ -24,7 +23,7 @@ class ReturnType extends Php7Backport\Visitor
 {
     public function leaveNode(Node $node)
     {
-        if (($node instanceof Function_ || $node instanceof ClassMethod)
+        if (($node instanceof FunctionLike)
             && isset($node->returnType))
         {
             return $this->tranformAndSave($node);
---
+++ tests/run.bat
@@ -0,0 +1,3 @@
+call ../vendor/bin/phpunit.bat .
+
+pause

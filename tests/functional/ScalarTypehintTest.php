3ade9a3a7fdf58d561df97437339178ea5d36ecd
Michael Haeuslmann <haeuslmann@gmail.com>
2017-07-16 22:01:28 +0200

    Remove scalar aliases and iterable

--- tests/functional/ScalarTypehintTest.php
+++ tests/functional/ScalarTypehintTest.php
@@ -5,10 +5,27 @@ require_once __DIR__ . '/BackporterFunctionalTestAbstract.php';
 
 class ScalarTypehintTest extends BackporterFunctionalTestAbstract
 {
-    public function testScalarTypeHint()
+    /**
+     * @dataProvider invalidTypeHintProvider
+     */
+    public function testScalarTypeHint(string $typeHint)
     {
-        $code = '<?php function foo(string $x, SomeClass $y) {}';
+        $code = '<?php function foo(' . $typeHint . ' $x, SomeClass $y) {}';
         $expected = '<?php function foo($x, SomeClass $y) {}';
         $this->assertEquals($expected, $this->backporter->port($code));
+    }
+
+
+    public function invalidTypeHintProvider() : array
+    {
+        return [
+            ['float'],
+            ['double'],
+            ['string'],
+            ['int'],
+            ['bool'],
+            ['boolean'],
+            ['iterable'],
+        ];
     }
 }

9194afbe4aee0abc825974eabf78d7e451bf2735
Michael Haeuslmann <haeuslmann@gmail.com>
2017-07-24 10:26:19 +0200

    Remove scalar aliases and iterable (#13)
    * Remove scalar aliases and iterable
    
    * Make changes PHP 5.6 compatible
    
    * Change const to private static
    
     - provide 3rd parameter for type safety in in_array

--- src/Bouda/Php7Backport/Visitor/ScalarTypehint.php
+++ src/Bouda/Php7Backport/Visitor/ScalarTypehint.php
@@ -12,18 +12,29 @@ use PhpParser\Node\Param;
 /**
  * Remove scalar typehint from function or method parameter.
  *
- * Example: 
+ * Example:
  * function foo(string $x, SomeClass $y) {...
  * becomes
  * function foo($x, SomeClass $y) {...
  */
 class ScalarTypehint extends Php7Backport\Visitor
 {
+    private static $INVALID_TYPE_HINTS = [
+        'int',
+        'float',
+        'double',
+        'string',
+        'bool',
+        'boolean',
+        'iterable',
+    ];
+
+
     public function leaveNode(Node $node)
     {
         if ($node instanceof Param
-            && isset($node->type->parts[0]) 
-            && in_array($node->type->parts[0], ['int', 'float', 'string', 'bool']))
+            && isset($node->type->parts[0])
+            && in_array($node->type->parts[0], self::$INVALID_TYPE_HINTS, true))
         {
             return $this->tranformAndSave($node);
         }
